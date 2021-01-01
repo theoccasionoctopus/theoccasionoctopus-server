ddc350e5b675ac98555359c532506adfae3f1fe0
James <james@jarofgreen.co.uk>
2021-01-01 09:15:11 +0000

    logs: Don't log 404's to Bugsnag

--- src/Kernel.php
+++ src/Kernel.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Kernel extends BaseKernel
 {
@@ -34,5 +35,19 @@ class Kernel extends BaseKernel
         } elseif (is_file($path = \dirname(__DIR__).'/config/routes.php')) {
             (require $path)($routes->withPath($path), $this);
         }
+    }
+
+    public function boot()
+    {
+        parent::boot();
+        $this->container->get('bugsnag')->registerCallback(function ($report) {
+            $originalError = $report->getOriginalError();
+            if (is_object($originalError) && is_a($originalError, NotFoundHttpException::class)) {
+                // We don't want bots scanning sites for wordpress etc to clutter up error logs
+                return false;
+            } else {
+                return true;
+            }
+        });
     }
 }
