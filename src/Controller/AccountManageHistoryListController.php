20fe5ac92c29f07e9d927eb5323a73fe3682f682
James <james@jarofgreen.co.uk>
2021-01-10 16:14:27 +0000

    ui: Start history section of account

---
+++ src/Controller/AccountManageHistoryListController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\History;
+use App\RepositoryQuery\TagRepositoryQuery;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use App\Entity\Account;
+use App\Entity\Tag;
+use App\Library;
+use App\Form\EventNewType;
+use Symfony\Component\HttpFoundation\Request;
+
+class AccountManageHistoryListController extends AccountManageController
+{
+    public function index($account_username, Request $request)
+    {
+        $this->build($account_username);
+
+        $doctrine = $this->getDoctrine();
+        $histories = $doctrine->getRepository(History::class)->findBy(['account'=>$this->account], ['created'=>'DESC'], 100);
+
+        return $this->render('account/manage/history/index.html.twig', $this->getTemplateVariables([
+            'account'=> $this->account,
+            'histories' => $histories,
+        ]));
+    }
+}
